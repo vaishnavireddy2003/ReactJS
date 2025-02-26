98a1a3198eb26a43b848839ffc62280b0f867a23
Vaishnavi Reddy <balgurivaishnavireddy@gmail.com>
2025-02-26 11:29:23 +0530

    Delete Microservices directory

--- Microservices/src/com/wipro/java/microservices/solid/BookInterface.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.java.microservices.solid;
-
-public interface BookInterface {
-	String getTitle();
-	String getAuthor();
-	int getYear();
-	double getPrice();
-	String getIsbn();
-}
